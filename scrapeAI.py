b2e5abdd7907cdb0f33b9267e4bfe0a767491a36
CodCodingCode <nathanyan2008p@gmail.com>
2024-03-29 12:09:43 -0400

    fixed error in code

--- scrapeAI.py
+++ scrapeAI.py
@@ -6,13 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from removeMonths import months
 import time
 
-list = ["OpenAI", "Deepmind", "Cohere"] #, "EleutherAI", "Mistral", "META", "Microsoft", "IBM", "Google", "Tesla"]
+list = ["OpenAI", "Deepmind", "Cohere", "EleutherAI", "Mistral", "META", "Microsoft", "IBM", "Google", "Tesla"]
 driver = webdriver.Chrome()
 
 
 
 possible_hackathons = []
-for i in range(3):
+for i in range(10):
     driver.get('https://www.google.com')
 
     WebDriverWait(driver, 10).until(
@@ -23,8 +23,8 @@ for i in range(3):
     input_element.clear()
     for month in months:
         try:
-            input_element.send_keys(f"{list[i]} Hackathon 2024 14+ {month}" + Keys.ENTER)
-            link = driver.find_element(By.PARTIAL_LINK_TEXT)
+            input_element.send_keys(f"{list[i]} Hackathon 2024 {month}" + Keys.ENTER)
+            link = driver.find_element(By.PARTIAL_LINK_TEXT, f"Hackathon 2024 {month}")
             link.click()
             possible_hackathons.append(driver.current_url)
         except:
