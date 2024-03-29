1ec8d85c85cb7272d24fe18b57ca75d908047c43
CodCodingCode <nathanyan2008p@gmail.com>
2024-03-29 11:38:09 -0400

    replaced scrape.py to scrapeAI.py for better understanding

---
+++ scrapeAI.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+list = ["OpenAI", "Deepmind", "Cohere"] #, "EleutherAI", "Mistral", "META", "Microsoft", "IBM", "Google", "Tesla"]
+driver = webdriver.Chrome()
+
+possible_hackathons = []
+for i in range(3):
+    driver.get('https://www.google.com')
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+    )
+
+    input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+    input_element.clear()
+    input_element.send_keys(f"{list[i]} Hackathon 2024 14+" + Keys.ENTER)
+    link = driver.find_element(By.PARTIAL_LINK_TEXT, "Hackathon")
+    link.click()
+    possible_hackathons.append(driver.current_url)
+    time.sleep(5)
+driver.quit()
+print(possible_hackathons)
